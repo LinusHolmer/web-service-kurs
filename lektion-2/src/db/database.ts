b85dce40105c3873fbe6a241b6d60e5f126f1dc0
LinusHolmer <linus.holmer@stud.sti.se>
2025-08-22 14:23:39 +0200

    Update database.ts

--- lektion-2/src/db/database.ts
+++ lektion-2/src/db/database.ts
@@ -1,46 +1,42 @@
 import { Db, MongoClient, ServerApiVersion } from "mongodb"
 import { validateSecret } from "../security/validateEnvt.js"
-
-let client: MongoClient
+// Runtime-Validation
+const uri: string = validateSecret(process.env.DB_CONNECTION_STRING)
+const dbName: string = validateSecret(process.env.DB_NAME)
+const client = new MongoClient(uri, {
+serverApi: {
+ version: ServerApiVersion.v1,
+ strict: true,
+ deprecationErrors: true,
+ },
+})
 let isConnected = false
-let dbName: string
 
+/** Attempts to connect to database.
+* Returns a promise.
+* Throws error at failed attempt. */
 export async function runDB(): Promise<void> {
-    if (isConnected) return
-
-    // Load env vars here, after dotenv has run
-    const uri = validateSecret(process.env.DB_CONNECTION_STRING)
-    dbName = validateSecret(process.env.DB_NAME)
-
-    client = new MongoClient(uri, {
-        serverApi: {
-            version: ServerApiVersion.v1,
-            strict: true,
-            deprecationErrors: true,
-        },
-    })
-
-    try {
-        await client.connect()
-        await client.db("admin").command({ ping: 1 })
-        isConnected = true
-        console.log("Db is up and running")
-    } catch (err) {
-        console.error(err)
-        throw err
-    }
+if (isConnected) return // prevent duplicate connects
+try {
+ await client.connect()
+ await client.db("admin").command({ ping: 1 })
+ isConnected = true
+ console.log("Db is up and running")
+ } catch (err) {
+ console.error(err)
+ throw err
+ }
 }
 
 export function getDB(): Db {
-    if (!isConnected) {
-        throw new Error("Tried to access DB before connecting")
-    }
-    return client.db(dbName)
+if (!isConnected) {
+ throw new Error(" Tried to access DB before connecting")
+ }
+return client.db(dbName)
+}
+export async function closeDB(): Promise<void> {
+await client.close()
+isConnected = false
+console.log("Mongodb connection closed")
 }
 
-export async function closeDB(): Promise<void> {
-    if (!client) return
-    await client.close()
-    isConnected = false
-    console.log("Mongodb connection closed")
-}
