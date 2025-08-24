6f446a4fb047e954bdfd818d9df8bb5ab04ecd93
LinusHolmer <linus.holmer@stud.sti.se>
2025-08-24 15:50:39 +0200

    Update index.ts

--- lektion-3/src/index.ts
+++ lektion-3/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config";
-import { closeDB, runDB } from "./db/database.js";
+import { closeDB, getDB, runDB } from "./db/database.js";
+import { Db, MongoClient, ServerApiVersion } from "mongodb";
 
 const app = express();
 const port: number = Number(process.env.PORT) || 3000;
@@ -24,13 +25,31 @@ async function startServer() {
 }
 startServer();
 
+app.get("/api/v1/database/comments/:username", async (req, res) => {
+  const db: Db = getDB();
+  const result = await db
+    .collection("comments")
+    .find({ name: req.params.username }) //Mercedes Tyler
+    .limit(25)
+    .toArray();
+
+  if (result.length === 0) {
+    res.status(404).send({ message: "Nothing was found" });
+    return;
+  }
+
+  res.send(result);
+});
+
+/*
 app.get("/", (req, res) => {
   res.send({ message: "Hello world!" });
 
-  /*
+
        res.send(new Buffer('test'));
-       */
+       
 });
+*/
 
 /*Start server on Port variable
 app.listen(port, address, () => {
