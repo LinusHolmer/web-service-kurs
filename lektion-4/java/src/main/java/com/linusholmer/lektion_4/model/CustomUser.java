fab18b25fe1829e50e0aa1a7ed4a1248cee78ace
LinusHolmer <linus.holmer@stud.sti.se>
2025-08-31 00:10:19 +0200

    Update CustomUser.java

--- lektion-4/java/src/main/java/com/linusholmer/lektion_4/model/CustomUser.java
+++ lektion-4/java/src/main/java/com/linusholmer/lektion_4/model/CustomUser.java
@@ -3,7 +3,8 @@ package com.linusholmer.lektion_4.model;
 public record CustomUser(
         String username,
         String password,
-        boolean accountEnabled
+        boolean accountEnabled,
+        int id
 ) {
 
 
