ac2921e2ebb68ff569f7ed3405ab56de8cd7cc5c
LinusHolmer <linus.holmer@stud.sti.se>
2025-09-26 13:22:03 +0200

    lektion 7

---
+++ lektion-7/src/main/java/com/example/lektion_7/model/Student.java
@@ -0,0 +1,40 @@
+package com.example.lektion_7.model;
+
+public class Student {
+    Long Id;
+    String FirstName;
+    String LastName;
+    String Email;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+}
